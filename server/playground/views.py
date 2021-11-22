805e6cdd1946c7604144ab75766ed0b508db1eff
Jure Sunic <juresunic@hotmail.com>
2021-11-22 14:29:24 +0000

    Added a JSONresponse

--- server/playground/views.py
+++ server/playground/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse
+
+ITEMS ={}
+
 # Request handler
-# Create your views here.
+# request -> response
+
+def say_hello(request):
+    # We can pull data from db, send emails, transform data
+    #return HttpResponse('Hello World')
+    response = JsonResponse({'foo': 'bar'})
+    response.status_code = 201
+    response.headers['Test'] = 120
+    return response
--- server/server/settings.py
+++ server/server/settings.py
@@ -31,12 +31,12 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'playground',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'playground',
 ]
 
 MIDDLEWARE = [
