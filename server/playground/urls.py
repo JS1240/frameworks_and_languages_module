3cc620464fd840ae0e62835913126c8123aca8b9
Jure Sunic <juresunic@hotmail.com>
2021-12-04 10:19:29 +0000

    Installed REST framework

--- server/playground/urls.py
+++ server/playground/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.contrib import admin
 from . import views
 
 # URLConfiguration
 urlpatterns = [
+    path('api-auth/', include('rest_framework.urls')),
+    path('admin', admin.site.urls),
     path('hello/', views.say_hello)
 ]
 
