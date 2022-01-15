b093b4bf6e2b4a8e005c2f3475034f1b3d95f11c
Jure Sunic <juresunic@hotmail.com>
2022-01-15 15:21:27 +0000

    Added admin calss to the server

--- server/playground/admin.py
+++ server/playground/admin.py
@@ -3,4 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from .models import Item
 
-admin.site.register(Item)
+
+@admin.site.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user_id', 'description', 'keywords', 'image']
