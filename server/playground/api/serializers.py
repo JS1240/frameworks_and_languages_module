e16b7ce8b20ff86ebf616afcc282f304037657c4
Jure Sunic <juresunic@hotmail.com>
2022-01-15 16:01:06 +0000

    Deleted unneeded folder.

--- server/playground/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-from playground.models import Item
-
-class ItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Item
-        fields = ('id', 'keywords', 'description', 'image', 'lat', 'lon', 'date_from', 'date_to')
--- server/playground/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import pathlib
-
-from .views import ItemListView, ItemDetailView
-
-urlpatterns = [
-    path('', ItemListView.as_view()),
-    path('<pk>', ItemDetailView.as_view())
-]
