e5cd1dc2b41b3384a4e32865c3b4e53d9e95263e
Jure Sunic <juresunic@hotmail.com>
2022-01-15 15:28:07 +0000

    Edited the models to fit the requirements.

--- server/playground/models.py
+++ server/playground/models.py
@@ -1,17 +1,22 @@
+from email.policy import default
 from django.db import models
+from django.db.models.fields.files import ImageField
+from django.contrib.postgres.fields import ArrayField
 # WE define models classes for the app
 # We use model classes to pull out data from the database and present it to user
 # Create your models here.
 
+
 class Item(models.Model):
-    id = models.CharField(max_length=120)
+    user_id = models.CharField(max_length=120)
     keywords = models.CharField(max_length=120)
-    description = models.CharField(max_length=250)
-    image = models.URLField('https://placekitten.com/200/300')
-    #lat = models.latitude(data.lat)
-    #lon = models.longitude(data.lon)
-    date_from = models.DateTimeField('date published')
-    date_to = models.DateTimeField('date available to')
+    description = models.CharField()
+    image = models.CharField(max_length=500, blank=True,
+                             null=True, default="https://placekitten.com/200/300")
+    lat = models.DecimalField(max_digits=12, decimal_places=9)
+    lon = models.DecimalField(max_digits=12, decimal_places=9)
+    date_from = models.DateTimeField()
+    date_to = models.DateTimeField()
 
     def _str_(self):
         return self.user_id
