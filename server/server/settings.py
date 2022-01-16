4565980c15e19fcc7b9c8e8f265e03c4a5bd9ef7
Jure Sunic <juresunic@hotmail.com>
2022-01-16 18:31:41 +0000

    Added code to the docker files.

--- server/server/settings.py
+++ server/server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--a_-2ek93)$2(ijt2=f#-!h2u-#&$$6a%$g#jge*dl-m-i+e&&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -77,29 +77,29 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
 #    'default': {
-
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-#        'NAME': 'database',
-
-#        'USER': 'postgres',
-
-#        'PASSWORD': 'admin',
-
-#        'HOST': 'localhost',
-
-#        'PORT': '5432',
-
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
 # }
+DATABASES = {
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'database',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'admin',
+
+        'HOST': '127.0.0.1',
+
+        'PORT': '5432',
+
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
